7cc40c2798de8b9fed8ae5089f13af997a7cb977
raff333 <nnn240.games@gmail.com>
2023-09-22 11:21:03 +0200

    x

--- js/gdpr.js
+++ js/gdpr.js
@@ -1,10 +1,7 @@
 class GDPR {
 
     constructor() {
-        this.showStatus();
-        this.showContent();
         this.bindEvents();
-
         if(this.cookieStatus() !== 'accept') this.showGDPR();
     }
 
@@ -12,62 +9,20 @@ class GDPR {
         let buttonAccept = document.querySelector('.gdpr-consent__button--accept');
         buttonAccept.addEventListener('click', () => {
             this.cookieStatus('accept');
-            this.showStatus();
-            this.showContent();
             this.hideGDPR();
         });
 
         let buttonReject = document.querySelector('.gdpr-consent__button--reject');
         buttonReject.addEventListener('click', () => {
             this.cookieStatus('reject');
-            this.showStatus();
-            this.showContent();
             this.hideGDPR();
         });
-
-//student uitwerking
-
-
-    }
-
-    showContent() {
-        this.resetContent();
-        const status = this.cookieStatus() == null ? 'not-chosen' : this.cookieStatus();
-        const element = document.querySelector(`.content-gdpr-${status}`);
-        element.classList.add('show');
-
-    }
-
-    resetContent(){
-        const classes = [
-            '.content-gdpr-accept',
-
-//student uitwerking
-
-            '.content-gdpr-not-chosen'];
-
-        for(const c of classes){
-            document.querySelector(c).classList.add('hide');
-            document.querySelector(c).classList.remove('show');
-        }
-    }
-
-    showStatus() {
-        document.getElementById('content-gpdr-consent-status').innerHTML =
-            this.cookieStatus() == null ? 'Niet gekozen' : this.cookieStatus();
     }
 
     cookieStatus(status) {
-
         if (status) localStorage.setItem('gdpr-consent-choice', status);
-
-//student uitwerking
-
         return localStorage.getItem('gdpr-consent-choice');
     }
-
-//student uitwerking
-
 
     hideGDPR(){
         document.querySelector(`.gdpr-consent`).classList.add('hide');
@@ -94,8 +49,6 @@ class GDPR {
         }
         return "";
     }
-
 }
 
 const gdpr = new GDPR();
-
